127b506b05fcb77b2f2c28bdef396cfb51c0dcb2
mapkeji <mapkeji26@gmail.com>
2018-06-13 16:03:05 +0300

    Separate API logic in component

---
+++ src/API.js
@@ -0,0 +1,57 @@
+/* global document, window */
+
+export default class VKApi {
+  constructor(apiId, options = {}) {
+    this.apiId = apiId;
+    this.options = {
+      version: 154,
+      onlyWidgets: true,
+      ...options
+    };
+
+    if (!apiId) {
+      throw new Error("You need to set apiId");
+    }
+
+    this.promise = null;
+
+    this.init();
+  }
+
+  async init() {
+    const {
+      apiId,
+      options: { version, onlyWidgets }
+    } = this;
+
+    if (this.promise) return this.promise;
+
+    this.promise = new Promise(resolve => {
+      const baseUrl = `https://vk.com/js/api/openapi.js?${version}`;
+
+      window.vkAsyncInit = () => {
+        window.VK.init({
+          apiId,
+          onlyWidgets
+        });
+
+        resolve(window.VK);
+      };
+
+      if (document.getElementById("vk-openapi")) {
+        return;
+      }
+
+      const script = document.createElement("script");
+
+      script.type = "text/javascript";
+      script.id = "vk-openapi";
+      script.src = baseUrl;
+      script.async = true;
+
+      document.head.appendChild(script);
+    });
+
+    return this.promise;
+  }
+}
--- src/VK.js
+++ src/VK.js
@@ -1,9 +1,13 @@
+/* global window */
 import React from "react";
 import PropTypes from "prop-types";
 
 import VKContext from "./VKContext";
+import VKApi from "./API";
 
-export class VK extends React.Component {
+let VKInstance = null;
+
+export default class VK extends React.Component {
   static propTypes = {
     apiId: PropTypes.number.isRequired,
     options: PropTypes.shape({
@@ -21,93 +25,42 @@ export class VK extends React.Component {
     onApiAvailable: () => {}
   };
 
-  state = {
-    vk: null
-  };
-
+  state = { vk: null };
   _mounted = true;
 
   componentDidMount() {
-    const { onApiAvailable } = this.props;
-    if (document.getElementById("vk-openapi")) {
-      this._mounted && this.setState({ vk: window.VK });
-    }
-
     if (
-      !!(
-        typeof window !== "undefined" &&
-        window.document &&
-        window.document.createElement
-      )
+      typeof window !== "undefined" &&
+      window.document &&
+      window.document.createElement
     ) {
-      this.init()
-        .then(api => {
-          onApiAvailable(api);
-          this._mounted && this.setState({ vk: api });
-        })
-        .catch(err => {
-          throw new Error(err);
-        });
+      this.VKinit().then(vk => {
+        if (this._mounted) this.setState({ vk });
+      });
     }
-  }
-
-  fetchScript = url => {
-    return new Promise((resolve, reject) => {
-      const el = document.createElement("script");
-      el.onload = resolve;
-      el.onerror = reject;
-      el.type = "text/javascript";
-      el.src = url;
-      el.async = true;
-      el.id = "vk-openapi";
-      document.getElementsByTagName("head")[0].appendChild(el);
-    });
-  };
-
-  init() {
-    if (this.loadingPromise) {
-      return this.loadingPromise;
-    }
-
-    this.loadingPromise = new Promise((resolve, reject) => {
-      const { apiId, options } = this.props;
-
-      if (!apiId) {
-        throw new Error("You need to set apiId");
-      }
-
-      window.vkAsyncInit = () => {
-        window.VK.init({
-          apiId: apiId,
-          onlyWidgets: options.onlyWidgets
-        });
-
-        resolve(window.VK);
-      };
-
-      if (document.getElementById("vk-openapi")) {
-        return;
-      }
-
-      this.fetchScript(
-        `https://vk.com/js/api/openapi.js?${options.version}`
-      ).catch(reject);
-    });
-
-    return this.loadingPromise;
   }
 
   componentWillUnmount() {
     this._mounted = false;
   }
 
+  async VKinit() {
+    const { apiId, options, onApiAvailable } = this.props;
+
+    if (!this.vk) {
+      this.vk = VKInstance || new VKApi(apiId, options);
+      VKInstance = this.vk;
+    }
+
+    const vk = await this.vk.init();
+    onApiAvailable(vk);
+    return vk;
+  }
+
   render() {
     const { vk } = this.state;
-
     return vk ? (
-      <VKContext.Provider value={vk}>
-        <React.Fragment>{this.props.children}</React.Fragment>
-      </VKContext.Provider>
+      <VKContext.Provider value={vk}>{this.props.children}</VKContext.Provider>
     ) : null;
   }
 }
