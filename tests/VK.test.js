276165990d20ac4cb46a753a7bfed2330c502015
mapkeji <mapkeji26@gmail.com>
2018-06-13 22:06:38 +0300

    Add jest and simple test

---
+++ tests/VK.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import VK from '../src';
+
+describe('Provider', () => {
+  test('mounted correctly', async done => {
+    const wrapper = await shallow(
+      <VK apiId="123456789"/>
+    );
+
+    expect(wrapper.instance()).toHaveProperty('_mounted', true);
+    done();
+  });
+});
