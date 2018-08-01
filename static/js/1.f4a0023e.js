c158456f0fa2f3c22e015724d59c7a87251d2771
mapkeji <mapkeji26@gmail.com>
2018-08-01 10:15:16 +0300

    Updates

---
+++ static/js/1.f4a0023e.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{110:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0}),t.default=void 0;var a,l=(a=n(2))&&a.__esModule?a:{default:a},o=n(111);var u=function(){return l.default.createElement("div",{className:"App-content"},l.default.createElement("div",{style:{textAlign:"center"}},l.default.createElement("h2",null,"Ooops! Page not found"),l.default.createElement(o.Link,{to:"/"},"Go Home")))};t.default=u}}]);
+//# sourceMappingURL=1.f4a0023e.js.map
