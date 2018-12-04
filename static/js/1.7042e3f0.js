1eda70d638db4100caf9620aeccf941e64f2409f
mapkeji <mapkeji26@gmail.com>
2018-12-04 13:01:44 +0300

    Updates

---
+++ static/js/1.7042e3f0.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{129:function(e,t,n){"use strict";n.r(t);var a=n(2),o=n.n(a),c=n(96);t.default=(()=>o.a.createElement("div",{className:"App-content"},o.a.createElement("div",{style:{textAlign:"center"}},o.a.createElement("h2",null,"Ooops! Page not found"),o.a.createElement(c.a,{to:"/"},"Go Home"))))}}]);
+//# sourceMappingURL=1.7042e3f0.js.map
