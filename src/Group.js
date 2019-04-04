93d82e189fba1f19c2be2fca32fab531e25d803a
mapkeji <mapkeji26@gmail.com>
2019-04-04 16:50:21 +0300

    Rewrite Group to hooks

--- src/Group.js
+++ src/Group.js
@@ -1,59 +1,51 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import PropTypes from "prop-types";
 import VKContext from "./VKContext";
 
-export default class Group extends React.Component {
-  static contextType = VKContext;
+const Group = ({ elementId, options, groupId, onJoin, onLeave }) => {
+  const { vk } = useContext(VKContext);
 
-  static propTypes = {
-    elementId: PropTypes.string,
-    groupId: PropTypes.number.isRequired,
-    options: PropTypes.shape({
-      height: PropTypes.number,
-      mode: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
-      no_cover: PropTypes.oneOf([0, 1]),
-      wide: PropTypes.oneOf([0, 1]),
-      color1: PropTypes.string,
-      color2: PropTypes.string,
-      color3: PropTypes.string,
-      pageImage: PropTypes.string
-    }),
-    onJoin: PropTypes.func,
-    onLeave: PropTypes.func
-  };
-
-  static defaultProps = {
-    elementId: "vk_groups",
-    options: {
-      width: "auto",
-      mode: 3,
-      no_cover: 1,
-      wide: 1
-    },
-    onJoin: () => {},
-    onLeave: () => {}
-  };
-
-  componentDidMount() {
-    this.mount();
-  }
-
-  componentWillUnmount() {
-    const { vk } = this.context;
-    vk.Observer.unsubscribe("widgets.groups.joined");
-    vk.Observer.unsubscribe("widgets.groups.leaved");
-  }
-
-  mount() {
-    const { vk } = this.context;
-    const { elementId, options, groupId, onJoin, onLeave } = this.props;
+  useEffect(() => {
     vk.Widgets.Group(elementId, options, groupId);
     vk.Observer.subscribe("widgets.groups.joined", onJoin);
     vk.Observer.subscribe("widgets.groups.leaved", onLeave);
-  }
 
-  render() {
-    const { elementId } = this.props;
-    return <div id={elementId} />;
-  }
-}
+    return () => {
+      vk.Observer.unsubscribe("widgets.groups.joined");
+      vk.Observer.unsubscribe("widgets.groups.leaved");
+    };
+  }, []);
+
+  return <div id={elementId} />;
+};
+
+Group.propTypes = {
+  elementId: PropTypes.string,
+  groupId: PropTypes.number.isRequired,
+  options: PropTypes.shape({
+    height: PropTypes.number,
+    mode: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    no_cover: PropTypes.oneOf([0, 1]),
+    wide: PropTypes.oneOf([0, 1]),
+    color1: PropTypes.string,
+    color2: PropTypes.string,
+    color3: PropTypes.string,
+    pageImage: PropTypes.string
+  }),
+  onJoin: PropTypes.func,
+  onLeave: PropTypes.func
+};
+
+Group.defaultProps = {
+  elementId: "vk_groups",
+  options: {
+    width: "auto",
+    mode: 3,
+    no_cover: 1,
+    wide: 1
+  },
+  onJoin: () => {},
+  onLeave: () => {}
+};
+
+export default Group;
