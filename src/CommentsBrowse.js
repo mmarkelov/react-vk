6b9c5e2f75014c1dff5d1cb14ec3a33165325ae1
mapkeji <mapkeji26@gmail.com>
2019-04-04 16:37:24 +0300

    Rewrite CommentsBrowse to hooks

--- src/CommentsBrowse.js
+++ src/CommentsBrowse.js
@@ -1,43 +1,36 @@
-import React from "react";
+import React, { useEffect, useContext } from "react";
 import PropTypes from "prop-types";
 import VKContext from "./VKContext";
 
-export default class CommentsBrowse extends React.Component {
-  static contextType = VKContext;
+const CommentsBrowse = ({ elementId, options }) => {
+  const { vk } = useContext(VKContext);
 
-  static propTypes = {
-    elementId: PropTypes.string,
-    options: PropTypes.shape({
-      width: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-      height: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-      limit: PropTypes.number,
-      mini: PropTypes.oneOf([0, 1, "auto"]),
-      norealtime: PropTypes.oneOf([0, 1])
-    })
-  };
-
-  static defaultProps = {
-    elementId: "vk_comments_browse",
-    options: {
-      height: 0,
-      limit: 10,
-      mini: "auto",
-      norealtime: 0
-    }
-  };
-
-  componentDidMount() {
-    this.mount();
-  }
-
-  mount() {
-    const { vk } = this.context;
-    const { elementId, options } = this.props;
+  useEffect(() => {
     vk.Widgets.CommentsBrowse(elementId, options);
-  }
+  }, []);
 
-  render() {
-    const { elementId } = this.props;
-    return <div id={elementId} />;
+  return <div id={elementId} />;
+};
+
+CommentsBrowse.propTypes = {
+  elementId: PropTypes.string,
+  options: PropTypes.shape({
+    width: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    height: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    limit: PropTypes.number,
+    mini: PropTypes.oneOf([0, 1, "auto"]),
+    norealtime: PropTypes.oneOf([0, 1])
+  })
+};
+
+CommentsBrowse.defaultProps = {
+  elementId: "vk_comments_browse",
+  options: {
+    height: 0,
+    limit: 10,
+    mini: "auto",
+    norealtime: 0
   }
-}
+};
+
+export default CommentsBrowse;
