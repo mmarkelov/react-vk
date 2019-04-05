99b94c554c20475dbdd52b3a38f834ae304df05a
mapkeji <mapkeji26@gmail.com>
2019-04-05 10:13:54 +0300

    Rewrite Playlist to hooks

--- src/Playlist.js
+++ src/Playlist.js
@@ -1,37 +1,30 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import PropTypes from "prop-types";
 import VKContext from "./VKContext";
 
-export default class Playlist extends React.Component {
-  static contextType = VKContext;
+const Playlist = ({ elementId, ownerId, playlistId, hash, options }) => {
+  const { vk } = useContext(VKContext);
 
-  static propTypes = {
-    elementId: PropTypes.string,
-    ownerId: PropTypes.number.isRequired,
-    playlistId: PropTypes.number.isRequired,
-    hash: PropTypes.string.isRequired,
-    options: PropTypes.shape({
-      width: PropTypes.number
-    })
-  };
-
-  static defaultProps = {
-    elementId: "vk_playlist",
-    options: {}
-  };
-
-  componentDidMount() {
-    this.mount();
-  }
-
-  mount() {
-    const { vk } = this.context;
-    const { elementId, ownerId, playlistId, hash, options } = this.props;
+  useEffect(() => {
     vk.Widgets.Playlist(elementId, -ownerId, playlistId, hash, options);
-  }
+  }, []);
 
-  render() {
-    const { elementId } = this.props;
-    return <div id={elementId} />;
-  }
-}
+  return <div id={elementId} />;
+};
+
+Playlist.propTypes = {
+  elementId: PropTypes.string,
+  ownerId: PropTypes.number.isRequired,
+  playlistId: PropTypes.number.isRequired,
+  hash: PropTypes.string.isRequired,
+  options: PropTypes.shape({
+    width: PropTypes.number
+  })
+};
+
+Playlist.defaultProps = {
+  elementId: "vk_playlist",
+  options: {}
+};
+
+export default Playlist;
