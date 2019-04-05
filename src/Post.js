833902920f5c89827a5c600d37d35b11e09f2bd9
mapkeji <mapkeji26@gmail.com>
2019-04-05 10:19:13 +0300

    Rewrite Post to hooks

--- src/Post.js
+++ src/Post.js
@@ -1,37 +1,30 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import PropTypes from "prop-types";
 import VKContext from "./VKContext";
 
-export default class Post extends React.Component {
-  static contextType = VKContext;
+const Post = ({ elementId, ownerId, postId, hash, options }) => {
+  const { vk } = useContext(VKContext);
 
-  static propTypes = {
-    elementId: PropTypes.string,
-    ownerId: PropTypes.number.isRequired,
-    postId: PropTypes.number.isRequired,
-    hash: PropTypes.string.isRequired,
-    options: PropTypes.shape({
-      width: PropTypes.number
-    })
-  };
-
-  static defaultProps = {
-    elementId: "vk_post",
-    options: {}
-  };
-
-  componentDidMount() {
-    this.mount();
-  }
-
-  mount() {
-    const { vk } = this.context;
-    const { elementId, ownerId, postId, hash, options } = this.props;
+  useEffect(() => {
     vk.Widgets.Post(elementId, ownerId, postId, hash, options);
-  }
+  }, []);
 
-  render() {
-    const { elementId } = this.props;
-    return <div id={elementId} />;
-  }
-}
+  return <div id={elementId} />;
+};
+
+Post.propTypes = {
+  elementId: PropTypes.string,
+  ownerId: PropTypes.number.isRequired,
+  postId: PropTypes.number.isRequired,
+  hash: PropTypes.string.isRequired,
+  options: PropTypes.shape({
+    width: PropTypes.number
+  })
+};
+
+Post.defaultProps = {
+  elementId: "vk_post",
+  options: {}
+};
+
+export default Post;
