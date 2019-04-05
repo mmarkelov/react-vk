3b051429db656b0af091733de503a9b4c404a6fb
mapkeji <mapkeji26@gmail.com>
2019-04-05 10:24:53 +0300

    Rewrite Subscribe to hooks

--- src/Subscribe.js
+++ src/Subscribe.js
@@ -1,57 +1,49 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import PropTypes from "prop-types";
 import VKContext from "./VKContext";
 
-export default class Subscribe extends React.Component {
-  static contextType = VKContext;
+const Subscribe = ({
+  elementId,
+  options,
+  ownerId,
+  onSubscribe,
+  onUnsubscribe
+}) => {
+  const { vk } = useContext(VKContext);
 
-  static propTypes = {
-    elementId: PropTypes.string,
-    options: PropTypes.shape({
-      mode: PropTypes.number,
-      soft: PropTypes.number
-    }),
-    ownerId: PropTypes.number.isRequired,
-    onSubscribe: PropTypes.func,
-    onUnsubscribe: PropTypes.func
-  };
-
-  static defaultProps = {
-    elementId: "vk_subscribe",
-    options: {
-      mode: 0,
-      soft: 0
-    },
-    onSubscribe: () => {},
-    onUnsubscribe: () => {}
-  };
-
-  componentDidMount() {
-    this.mount();
-  }
-
-  componentWillUnmount() {
-    const { vk } = this.context;
-    vk.Observer.unsubscribe("widgets.subscribed");
-    vk.Observer.unsubscribe("widgets.unsubscribed");
-  }
-
-  mount() {
-    const { vk } = this.context;
-    const {
-      elementId,
-      options,
-      ownerId,
-      onSubscribe,
-      onUnsubscribe
-    } = this.props;
+  useEffect(() => {
     vk.Widgets.Subscribe(elementId, options, ownerId);
     vk.Observer.subscribe("widgets.subscribed", onSubscribe);
     vk.Observer.subscribe("widgets.unsubscribed", onUnsubscribe);
-  }
 
-  render() {
-    const { elementId } = this.props;
-    return <div id={elementId} />;
-  }
-}
+    return () => {
+      vk.Observer.unsubscribe("widgets.subscribed");
+      vk.Observer.unsubscribe("widgets.unsubscribed");
+    };
+  }, []);
+
+  return <div id={elementId} />;
+};
+
+Subscribe.propTypes = {
+  elementId: PropTypes.string,
+  options: PropTypes.shape({
+    mode: PropTypes.number,
+    soft: PropTypes.number
+  }),
+  ownerId: PropTypes.number.isRequired,
+  onSubscribe: PropTypes.func,
+  onUnsubscribe: PropTypes.func
+};
+
+Subscribe.defaultProps = {
+  elementId: "vk_subscribe",
+  options: {
+    mode: 0,
+    soft: 0
+  },
+  onSubscribe: () => {},
+  onUnsubscribe: () => {}
+};
+
+export default Subscribe;
