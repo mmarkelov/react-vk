12ac5059da94a30233db8eb0ab660c8e54a325c2
mapkeji <mapkeji26@gmail.com>
2019-04-05 10:21:37 +0300

    Rewrite Recommended to hooks

--- src/Recommended.js
+++ src/Recommended.js
@@ -1,45 +1,38 @@
-import React from "react";
+import React, { useEffect, useContext } from "react";
 import PropTypes from "prop-types";
 import VKContext from "./VKContext";
 
-export default class Recommended extends React.Component {
-  static contextType = VKContext;
+const Recommended = ({ elementId, options }) => {
+  const { vk } = useContext(VKContext);
 
-  static propTypes = {
-    elementId: PropTypes.string,
-    options: PropTypes.shape({
-      limit: PropTypes.number,
-      max: PropTypes.number,
-      period: PropTypes.string,
-      verb: PropTypes.number,
-      sort: PropTypes.string,
-      target: PropTypes.string
-    })
-  };
-
-  static defaultProps = {
-    elementId: "vk_recommend",
-    options: {
-      limit: 5,
-      max: 20,
-      period: "week",
-      sort: "friend_like",
-      target: "parent"
-    }
-  };
-
-  componentDidMount() {
-    this.mount();
-  }
-
-  mount() {
-    const { vk } = this.context;
-    const { elementId, options } = this.props;
+  useEffect(() => {
     vk.Widgets.Recommended(elementId, options);
-  }
+  }, []);
 
-  render() {
-    const { elementId } = this.props;
-    return <div id={elementId} />;
+  return <div id={elementId} />;
+};
+
+Recommended.propTypes = {
+  elementId: PropTypes.string,
+  options: PropTypes.shape({
+    limit: PropTypes.number,
+    max: PropTypes.number,
+    period: PropTypes.string,
+    verb: PropTypes.number,
+    sort: PropTypes.string,
+    target: PropTypes.string
+  })
+};
+
+Recommended.defaultProps = {
+  elementId: "vk_recommend",
+  options: {
+    limit: 5,
+    max: 20,
+    period: "week",
+    sort: "friend_like",
+    target: "parent"
   }
-}
+};
+
+export default Recommended;
